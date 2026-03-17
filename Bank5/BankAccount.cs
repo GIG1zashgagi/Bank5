9dd76f355c6358d659ab4567089a520f56fbcaaa
234970 <234970@KB37-120-C06>
2026-03-17 13:38:13 +0300

    Тест 1: Проверка корректного уменьшения баланса

---
+++ Bank5/BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        private BankAccount() { }
+
+        /// <param name="customerName">Имя владельца счета</param>
+        /// <param name="balance">Начальный баланс счета</param>
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        /// <param name="amount">Сумма для снятия</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+    
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount,
+                    "Сумма снятия превышает баланс");
+            }
+
+            if (amount < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount,
+                    "Сумма снятия не может быть отрицательной");
+            }
+
+            m_balance -= amount; 
+        }
+
+        /// <param name="amount">Сумма для внесения</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount,
+                    "Сумма внесения не может быть отрицательной");
+            }
+
+            m_balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Roman Abramovich", 11.99);
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ BankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankTests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("588b032d-872f-4c0b-86d5-161b5743e847")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
